d1e56b9fbec68b90bd7e337c5b10fcaaa510c538
Jewel-Thomas <80634999+Jewel-Thomas@users.noreply.github.com>
2022-08-21 19:40:53 +0530

    Asset Imports

---
+++ Assets/Scripts/FPS_Display.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS_Display : MonoBehaviour
+{
+   public Text fpsText;
+    float deltaTime = 0.0f;
+	int w = Screen.width, h = Screen.height;
+
+ 
+	void Update()
+	{
+		deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+		fpsText.text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+	}
+}
