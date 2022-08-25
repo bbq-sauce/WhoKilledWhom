22d3dc3704e4ee0e3e2b5bbe8363aedfe8dc3ade
Fiza <fizasameer88@gmail.com>
2022-08-25 18:56:12 +0530

    Stuff

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DoorScript : MonoBehaviour
+{
+    public void Load_Next_Scene(Collider other)
+    {
+        SceneManager.LoadScene(2);
+    }
+}
+
+
