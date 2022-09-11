fe8e5b5aafc9916b0cd70eb7f3f38002eda98cd8
Jewel-Thomas <80634999+Jewel-Thomas@users.noreply.github.com>
2022-09-11 21:38:56 +0530

    changes

--- Assets/Scripts/MobileInput.cs
+++ Assets/Scripts/MobileInput.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MobileInput : MonoBehaviour
 {
+    public Animator showAnimator;
+    public bool isShowing = false;
     // public DoorScript doorScript;
     // Update is called once per frame
     void Update()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            DoorTouch();
+        }
+        if(SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            LicenseViewTouch();
+        }
+        
+
+#if UNITY_EDITOR
+
+        if(SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            DoorClick();
+        }
+        if(SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            LicenseViewClick();
+        }
+        
+        
+#endif       
+    }
+
+    void DoorTouch()
     {
         if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
@@ -25,9 +55,10 @@ public class MobileInput : MonoBehaviour
                 }
             }
         }
+    }
 
-#if UNITY_EDITOR
-
+    void DoorClick()
+    {
         if(Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -44,6 +75,66 @@ public class MobileInput : MonoBehaviour
                 }
             }
         }
-#endif       
     }
+
+    void LicenseViewTouch()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if((hit.collider != null) && hit.transform.gameObject.tag == "DL")
+                {
+                    if(Vector3.Distance(hit.transform.gameObject.transform.position,this.transform.position) < 1.5f)
+                    {
+                        if(!isShowing)
+                        {
+                            showAnimator.SetTrigger("ShowDL");
+                            isShowing = !isShowing;
+                        }
+                        
+                    }
+                }
+                else if(isShowing)
+                {  
+                    showAnimator.SetTrigger("RemoveDL");
+                    isShowing = !isShowing;      
+                }
+            }
+        }
+    }
+
+    void LicenseViewClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if((hit.collider != null) && hit.transform.gameObject.tag == "DL")
+                {
+                    if(Vector3.Distance(hit.transform.gameObject.transform.position,this.transform.position) < 1.5f)
+                    {
+                        if(!isShowing)
+                        {
+                            showAnimator.SetTrigger("ShowDL");
+                            isShowing = !isShowing;
+                        }
+                        
+                    }
+                }
+                else if(isShowing)
+                {  
+                    showAnimator.SetTrigger("RemoveDL");
+                    isShowing = !isShowing;      
+                }
+            }
+        }
+    }
+
 }
