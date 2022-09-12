f43c36882768d5e145f77fb50700b0e0e510cac7
Jewel-Thomas <80634999+Jewel-Thomas@users.noreply.github.com>
2022-09-12 22:41:48 +0530

    inventory

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventoryManager : MonoBehaviour
+{
+   public static InventoryManager instance;
+   public List<Item> items = new List<Item>();
+   public Transform itemContent;
+   public GameObject inventoryItem;
+
+   void Awake()
+   {
+        instance = this;
+   }
+
+   public void Add(Item item)
+   {
+        items.Add(item);
+   }
+
+   public void ListItems()
+   {
+        foreach (Transform item in itemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in items)
+        {
+            GameObject obj = Instantiate(inventoryItem, itemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+        }
+   }
+}
---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public GameObject player;
+    public Item item;
+    public string tagName;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void Pickup()
+    {
+        InventoryManager.instance.Add(item);
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if((hit.collider != null) && hit.transform.gameObject.tag == tagName)
+                {
+                    if(Vector3.Distance(player.transform.position,transform.position) < 3f)
+                    {
+                        Pickup();
+                    }
+                }
+            }
+        }
+    }
+
+#if UNITY_EDITOR
+    void OnMouseDown()
+    {
+        if(Vector3.Distance(player.transform.position,transform.position) < 3f)
+        {
+            Pickup();
+        }
+    }
+
+#endif
+}
