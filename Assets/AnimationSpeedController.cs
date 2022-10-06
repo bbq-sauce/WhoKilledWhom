2597b5bfd8c08c6730b2d4cdad304e2eb04b8465
Jewel-Thomas <80634999+Jewel-Thomas@users.noreply.github.com>
2022-10-07 01:41:31 +0530

    ghost

---
+++ Assets/AnimationSpeedController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSpeedController : MonoBehaviour
+{
+    public Animator animator;
+    public float animationSpeed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("AnimationSpeed",2,2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void AnimationSpeed()
+    {
+        animationSpeed = Random.Range(0.3f,2);
+        animator.speed = animationSpeed;
+    }
+}
