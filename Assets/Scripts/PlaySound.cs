55e65249cb72c07798a7ff20e18263715e2309c0
Adithya N Biya <adithyanbiya@gmail.com>
2022-10-08 18:49:31 +0530

    Soundtrigger1

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioClip SoundToPlay;
+    public float volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+
+        void OnTriggerEnter()
+        {
+            if (!alreadyPlayed)
+            {
+                audio.PlayOneShot(SoundToPlay, volume);
+                alreadyPlayed = true;
+            }
+        }
+    }
+
+}
